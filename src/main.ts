abe98df6ced9e3f81d68dab0427db2dbac7d558e
wahidfebr <wahidfebr@gmail.com>
2024-03-14 13:56:45 +0700

    feat: add env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const APP_HOST = configService.get<string>("APP_HOST") || "localhost";
+  const APP_PORT = configService.get<number>("APP_PORT") || 3000;
+
+  await app.listen(APP_PORT, APP_HOST, () => {
+    console.log(`HTTP Server started at http://${APP_HOST}:${APP_PORT}`);
+  });
 }
 bootstrap();
